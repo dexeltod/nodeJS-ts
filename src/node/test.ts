069c6ef1be4e028d0a326057ba1fef9ab6bad140
dexeltod <69011194+dexeltod@users.noreply.github.com>
2024-01-05 21:09:03 +0200

    get post

--- src/node/test.ts
+++ src/node/test.ts
@@ -1,9 +1,20 @@
 import express from "express"
 
-const a = express()
-console.log("adad")
+const application = express()
+application.use(express.json())
 
-a.get("/",(req, res) => {
-	console.log("poehali blya")
-	console.log("4e")
+application.get("/", (req, res) => {
+	res.send("чё?")
+})
+
+application.post('/authentication/login', (req, res) => {
+	const body = req.body
+
+	res.json({
+		isHuyUp: true,
+	})
+})
+
+application.listen(1488, () => {
+	console.log("Работаем братья")
 })
