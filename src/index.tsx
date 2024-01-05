0127175a57fdf5850cbf19ccf68f361fd1c2e596
dexeltod <69011194+dexeltod@users.noreply.github.com>
2024-01-05 20:07:11 +0200

    fix node typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+interface InitialState {
+	count: number;
+}
+
+interface IAction {
+	type: string
+	payload?: any
+}
+
+const initialState: InitialState = {
+	count: 0,
+}
+
+const reducer = (state: InitialState = initialState, action: IAction) => {
+	switch (action.type) {
+		case "ADD":
+			return state.count += action.payload
+		case "DECREASE":
+			return state.count -= action.payload
+		case "SET":
+			return state.count = action.payload
+	}
+}
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App/>
+		</Provider>
+
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/node/test.ts
@@ -0,0 +1,9 @@
+import express from "express"
+
+const a = express()
+console.log("adad")
+
+a.get("/",(req, res) => {
+	console.log("poehali blya")
+	console.log("4e")
+})
